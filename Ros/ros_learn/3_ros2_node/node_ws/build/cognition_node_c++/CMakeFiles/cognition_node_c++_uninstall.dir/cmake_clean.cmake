294542888fba22ce4110c20f70606cbb38f0bfbb
SHUNLU-1 <3527431075@qq.com>
2022-03-10 21:25:24 +0800

    delete file

--- Ros/ros_learn/3_ros2_node/node_ws/build/cognition_node_c++/CMakeFiles/cognition_node_c++_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cognition_node_c++_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cognition_node_c++_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Ros/ros_learn/3_ros2_node/node_ws/build/cognition_node_c++/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amber/code/Notes/Ros/3_ros2_node/node_ws/src/cognition_node_c++
-# Build directory: /home/amber/code/Notes/Ros/3_ros2_node/node_ws/build/cognition_node_c++
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
